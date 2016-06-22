e1e89e0c32296a446096446d1759b59a20107121
AlexanderD <sky1plus1@gmail.com>
2016-06-22 21:33:31 +0200

    *test*

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
+$cyty = array (
+ 	'city' = array(
+		'name' => 'Kharkiv',
+		'population' => 100500,
+ ),
 
-$loc = "Kh";
+ 	'city1' = array(
+		'name' => 'Lviv',
+		'population' => 200500
+ ),
 
+ 	'city2' = array(
+		'name' => 'Poltava',
+		'population' => 300500
+ )
+);
 
-
-echo "hello world";
+echo ( )
