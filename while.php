95dd4801b3fe04e9ad74d698a3e5e3a31ab916ba
AlexanderD <sky1plus1@gmail.com>
2016-07-05 19:44:10 +0200

    start

---
+++ while.php
@@ -0,0 +1,32 @@
+<?php
+
+$i = 10;
+if ($i < 3) {
+    echo $i. "я сижу дома";
+    echo "<br />";
+} elseif ($i < 5) {
+    echo $i. "я хожу в садик";
+    echo "<br />";
+
+} elseif ($i < 6) {
+    echo $i. "я хожу в садик";
+    echo "<br />";
+
+} elseif ($i < 17) {
+    echo $i. "я хожу в школу";
+    echo "<br />";
+
+} elseif ($i < 21) {
+    echo $i. "я хожу в универ";
+    echo "<br />";
+
+} elseif ($i < 60) {
+    echo $i. "я хожу на работу";
+    echo "<br />";
+
+} else ($i > 60 ) {
+    echo $i. "я отдыхаю";
+    echo "<br />";
+};
+
+
