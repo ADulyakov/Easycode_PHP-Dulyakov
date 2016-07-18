b43d9f177439ab9830b9767e6bd79b01ca5fa2a0
AlexanderD <sky1plus1@gmail.com>
2016-07-19 01:44:53 +0300

    var1

--- test1.php
+++ test1.php
@@ -1,11 +1,35 @@
 <?php
-$array = array (1,2,3);
+//$array = array (1,2,3);
+//
+//for($i=0; $i < count($array); $i++ ) {
+//
+//    $result = $array[$i] * 2;
+//
+//    echo $result;
+//
+//
+//}
 
-for($i=0; $i < count($array); $i++ ) {
+$rand_array = [];
+$ir = 0;
 
-    $result = $array[$i] * 2;
+while ($ir < 50){
+    $rand_array[] = rand (1,50);
+    $ir++;
+};
+echo '<pre>';
+print_r($rand_array);
+echo '</pre>';
+echo '<br />';
+echo '<hr>';
 
-    echo $result;
+$result = [];
+foreach ($rand_array as $key => $value){
+    if ($key % 5 == 0 ) {
+        $result[] = $value;
+    }
+};
 
-
-}
+echo '<pre>';
+print_r($result);
+echo '</pre>';
