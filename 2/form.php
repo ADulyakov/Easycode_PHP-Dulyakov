dc534f623465e644c26ad887f94479366e7db865
AlexanderD <sky1plus1@gmail.com>
2016-08-03 20:33:56 +0300

    up

--- 2/form.php
+++ 2/form.php
@@ -1,3 +1,8 @@
+<?php
+$date = date('Y-m-d H:i:s');
+setcookie('lastTime', $date);
+?>
+
 <?php if (isset($_COOKIE['login'])): ?>
     Привет, <?= $_COOKIE['login'] ?>!
     <br />
@@ -7,7 +12,10 @@
     <form action="cookie_handler.php">
         Login: <input type="text" name="login" title="1"/>
         Жизнь куки: <input type="text" name="life" title="2"/>
-            
+
         <input type="submit" value="Send" />
     </form>
 <?php endif; ?>
+<br />
+Дата последнего посищения: <?= $_COOKIE['lastTime'] ?>;
+
